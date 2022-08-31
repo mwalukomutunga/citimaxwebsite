a7b947a59a614bc1b97a9dfdcf69cdc1f62f03aa
Mwaluko <mwalukomutunga@gmail.com>
2022-08-31 03:09:19 +0300

    script chang

--- next.config.js
+++ next.config.js
@@ -1,7 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
+  reactStrictMode: false,
+  basePath: '',
+  compress: false,
   swcMinify: true,
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
 }
 
 module.exports = nextConfig
